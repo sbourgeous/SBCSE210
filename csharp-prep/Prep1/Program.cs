d1c2b1ae45bb3d18a4d978f484a2a0d105b964bc
sbourgeous <bou20009@byui.edu>
2025-01-10 18:35:28 -0700

    Added spacing.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,9 +6,11 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-        
+
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
+
+        Console.WriteLine("");
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
