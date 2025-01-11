2a861ea2bef2acc6901b3df6fee0459419001444
sbourgeous <bou20009@byui.edu>
2025-01-10 19:11:10 -0700

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int percentNumber = int.Parse(gradePercent);
+
+        string letter = "";
+
+        if (percentNumber >= 93)
+        {
+            letter = "A";
+        }
+        else if (percentNumber >= 90)
+        {
+            letter = "A-";
+        }
+        else if (percentNumber >= 87)
+        {
+            letter = "B+";
+        }
+        else if (percentNumber >= 83)
+        {
+            letter = "B";
+        }
+        else if (percentNumber >= 80)
+        {
+            letter = "B-";
+        }
+        else if (percentNumber >= 77)
+        {
+            letter = "C+";
+        }
+        else if (percentNumber >= 73)
+        {
+            letter = "C";
+        }
+        else if (percentNumber >= 70)
+        {
+            letter = "C-";
+        }
+        else if (percentNumber >= 67)
+        {
+            letter = "D+";
+        }
+        else if (percentNumber >= 63)
+        {
+            letter = "D";
+        }
+        else if (percentNumber >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade: {letter}");
+
+        if (percentNumber >= 70)
+        {
+            Console.WriteLine("You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class: Retake is required.");
+        }
     }
 }
