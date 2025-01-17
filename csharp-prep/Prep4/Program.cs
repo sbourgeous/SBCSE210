7b08a0a6b95d6d3ef2352aa525cb38dfa7b16c97
sbourgeous <bou20009@byui.edu>
2025-01-17 13:25:08 -0700

    Prep 4 completed.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int enterNumber = -1;
+        while(enterNumber != 0)
+        {
+            Console.Write("Please enter a number (0 to quit): ");
+
+            string respone = Console.ReadLine();
+            enterNumber = int.Parse(respone);
+
+            if (enterNumber != 0)
+            {
+                numbers.Add(enterNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is {sum}.");
+
+        float avg = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {avg}.");
+
+        int max = numbers [0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The maximum is {max}.");
     }
 }
