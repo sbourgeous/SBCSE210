19eeec0fdcf80fe63cde7b7e14efdf935ae2169a
sbourgeous <bou20009@byui.edu>
2025-01-17 12:26:49 -0700

    Sanbox and prep part 3 updates

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int magicGuess = -1;
+
+        while (magicGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            magicGuess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > magicGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < magicGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }      
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // adjective = adj
+        string adj = GetAdj();
+        string noun = GetNoun();
+        
+        int number = Multiply(3, 4);
+
+        Console.WriteLine($"I looked out the window and saw a {number} {adj} {noun}s.");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+
+        return product;
+    }
+
+    static string GetAdj()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("yellow");
+        words.Add("big");
+        words.Add("small");
+
+        string adj = words[2];
+
+        return adj;
+    }
+
+    static string GetNoun()
+    {
+        string noun = "bird";
+
+        return noun;
     }
 }
