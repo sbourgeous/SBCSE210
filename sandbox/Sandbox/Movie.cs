6db2b33404a2f99198ca571813cd408383b36f3f
sbourgeous <bou20009@byui.edu>
2025-01-21 16:16:14 -0700

    W03 Tuesday CW.

---
+++ sandbox/Sandbox/Movie.cs
@@ -0,0 +1,14 @@
+public class Movie
+{
+    // Member Variables:
+    public string _title;
+    public int _year;
+    public int _runtime;
+    public string _rating;
+
+    // Member Function (Are called "Methods"):
+    public void Display ()
+    {
+        Console.WriteLine(_title);
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,42 +1,33 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // adjective = adj
-        string adj = GetAdj();
-        string noun = GetNoun();
-        
-        int number = Multiply(3, 4);
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._runtime = 150;
+        favoriteMovie._rating = "PG";
 
-        Console.WriteLine($"I looked out the window and saw a {number} {adj} {noun}s.");
+        Movie otherMovie =  new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._runtime = 162;
+        otherMovie._rating = "PG-13";
+
+        // Linked with Member Function:
+        favoriteMovie.Display();
+
+        // Linked with Member Variables:
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
     }
 
-    static int Multiply(int number1, int number2)
+    static void DisplayMovie(Movie aMovie)
     {
-        int product = number1 * number2;
-
-        return product;
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
 
-    static string GetAdj()
-    {
-        List<string> words = new List<string>();
-        words.Add("blue");
-        words.Add("yellow");
-        words.Add("big");
-        words.Add("small");
-
-        string adj = words[2];
-
-        return adj;
-    }
-
-    static string GetNoun()
-    {
-        string noun = "bird";
-
-        return noun;
-    }
 }
