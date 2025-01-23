f1d4706bd44223e7d2a308150ff2798df09d7ed5
sbourgeous <bou20009@byui.edu>
2025-01-22 18:19:05 -0700

    W03 Prepare Learning Activity completion.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Lanscaper";
+        job1._company = "Yellowstone Bear World";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Customer Service Representative";
+        job2._company = "The Occasions Group";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Samuel Bourgeous";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
