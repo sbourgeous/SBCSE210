bb9971975ff0fbe54114389765a954e5d9804688
sbourgeous <bou20009@byui.edu>
2025-02-19 19:02:30 -0700

    W07 Prepare: Learning Activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,27 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic) 
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetTextBookSection()
+    {
+        return _textbookSection;
+    }
+
+    public string GetProblems()
+    {
+        return _problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
